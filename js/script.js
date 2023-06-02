4c115354c5f0a203601df8aa9ca0ba11e4dd10a1
Purice Victor <pvictor0110@gmail.com>
2023-06-02 14:56:47 +0300

    weatheradar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body'); 
 
 const container = document.querySelector('.container'); 
 
@@ -13,24 +14,27 @@ const weatherMain = document.querySelector('.weather-main');
 
 const mainImg = document.querySelector('.main-img');
 const imgMain = document.querySelector('.img_main'); 
-const mainTemp= document.querySelector('.main-temperature'); 
+const mainTemp = document.querySelector('.main-temperature'); 
 const mainWeather = document.querySelector('.main-weather'); 
 
 
 const weatherAdd = document.querySelector('.weather-add'); 
 
-const addHumidity = document.querySelector('.add-humidity'); 
-const humidityTitle = document.querySelector('.humidity-title');
 const humidityValue = document.querySelector('.humidity-value'); 
-
-const addVisibility = document.querySelector('.add-visibility'); 
-const visibilityTitle = document.querySelector('.visibility-title');
 const visibilityValue = document.querySelector('.visibility-value'); 
-
-const addWind = document.querySelector('.add-wind'); 
-const windTitle = document.querySelector('.wind-title');
 const windValue = document.querySelector('.wind-value'); 
 
+const theme = document.querySelector('.theme'); 
+
+
+//theme changing
+function themeChange() {
+    body.classList.toggle('themeBody'); 
+    searchInput.classList.toggle('themeSearch')
+}
+theme.addEventListener("click", themeChange); 
+
+//city search
 searchButton.addEventListener("click", () => {
     
 
@@ -45,29 +49,22 @@ searchButton.addEventListener("click", () => {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`).then(response => response.json()).then(json => {
         if(json.cod === '404') {
-
-            // weather.style.display = 'block';
             mainWeather.innerHTML = 'Not found'; 
-
             mainImg.style.display = 'none'; 
             mainTemp.style.display = 'none';
             weatherAdd.style.display = 'none'
-            
-                    
         }
 
-            // weather.style.display = 'block'; 
-            
             switch(json.weather[0].main) {
                 case 'Clouds':
                     imgMain.src = 'img/clouds.png';
                     mainWeather.innerHTML = 'Clouds';
-                    break;
+                break;
 
                 case 'Rain':
                     imgMain.src = 'img/rain.png';
                     mainWeather.innerHTML = 'Rain';
-                    break;
+                break;
 
                 case 'Drizzle':
                     imgMain.src = 'img/rain.png';
@@ -77,34 +74,32 @@ searchButton.addEventListener("click", () => {
                 case 'Thunderstorm' :
                     imgMain.src = 'img/Thunderstorm.png';
                     mainWeather.innerHTML = 'Thunderstorm';
-                    break;
+                break;
 
                 case 'Fog' :
                     imgMain.src = 'img/fog.png';
                     mainWeather.innerHTML = 'Fog';
-                    break;
+                break;
 
                 case 'Mist' :
-                imgMain.src = 'img/fog.png';
-                mainWeather.innerHTML = 'Mist';
+                    imgMain.src = 'img/fog.png';
+                    mainWeather.innerHTML = 'Mist';
                 break;
 
                 case 'Haze' :
-                imgMain.src = 'img/fog.png';
-                mainWeather.innerHTML = 'Haze';
+                    imgMain.src = 'img/fog.png';
+                    mainWeather.innerHTML = 'Haze';
                 break; 
 
                 case 'Clear':
                     imgMain.src = 'img/clear.png';
-                    
                     mainWeather.innerHTML = 'Clear';
-                    break;
+                break;
 
                 case 'Snow':
                     imgMain.src = 'img/snow.png';
-            
                     mainWeather.innerHTML = 'Snow';
-                    break;
+                break;
                 
                 default:
                     imgMain.src = '';   
@@ -117,37 +112,13 @@ searchButton.addEventListener("click", () => {
 
             mainImg.style.display = ''; 
             mainTemp.style.display = '';
-            weatherAdd.style.display = ''
+            weatherAdd.style.display = '';
 
-            
-            console.log(json)
+            console.log(json);
 
     }); 
     
 })
-
-
-
-// Задайте смещение времени в минутах
-var timezoneOffset = -300; // Например, -300 соответствует UTC-5
-
-// Создайте новый объект Date
-var currentTime = new Date();
-
-// Получите текущее время в миллисекундах
-var currentTimestamp = currentTime.getTime();
-
-// Вычислите смещение времени в миллисекундах
-var offsetMilliseconds = timezoneOffset * 60000;
-
-// Получите новое время, добавив смещение к текущему времени
-var targetTimestamp = currentTimestamp + offsetMilliseconds;
-
-// Создайте новый объект Date с новым временем
-var targetTime = new Date(targetTimestamp);
-
-// Выведите время в определенном часовом поясе
-console.log(targetTime);
 
 
 
