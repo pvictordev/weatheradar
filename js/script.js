7ac19f8c6000f43bbbf684d849ec22ae3e14c02f
Purice Victor <pvictor0110@gmail.com>
2023-06-28 14:02:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -37,34 +37,121 @@ theme.addEventListener("click", themeChange);
   //current location 
     const geoApi = 'AIzaSyBlVmT_ycGpsJg4LVbHA0IDJ5dZT4-fIPc'; 
     function locationClick() {
+  
         navigator.geolocation.getCurrentPosition(
             function(position) {
                 const geoLocation = `latlng=${position.coords.latitude}, ${position.coords.longitude}`
                 //console.log(position.coords)
-
+    
                 fetch(`https://maps.googleapis.com/maps/api/geocode/json?${geoLocation}&key=${geoApi}`).then(response => response.json()).then(json => {
-                    let currentLoc = json.results[6].address_components[1].long_name
-                    console.log(currentLoc)
+
+                    return json.results[6].address_components[1].long_name
+                    
                 })
             }
         )
-
+        
     }
-    locationIcon.addEventListener("click", locationClick); 
+locationIcon.addEventListener("click", locationClick); 
 
 
 
+// locationIcon.addEventListener("click", () => {
 
+//     const location = '';
+    
+//     if(location == '') return;
+    
+//     const APIkey = '80f6af54d5ac9494f7c6db394b035563'; 
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`).then(response => response.json()).then(json => {
+        
+//         if(json.cod === '404') {
+//             mainWeather.innerHTML = 'Not found'; 
+//             mainImg.style.display = 'none'; 
+//             mainTemp.style.display = 'none';
+//             weatherAdd.style.display = 'none'; 
+  
+//             container.classList.remove('slideDown'); 
+//             container.classList.add('slideUp');     
+            
+//         }
+           
+//         else {
+//             container.classList.remove('slideUp'); 
+//             container.classList.add('slideDown'); 
+         
+          
+//             switch(json.weather[0].main) {
+//                 case 'Clouds':
+//                     imgMain.src = 'img/clouds.png';
+//                     mainWeather.innerHTML = 'Clouds';
+//                 break;
+
+//                 case 'Rain':
+//                     imgMain.src = 'img/rain.png';
+//                     mainWeather.innerHTML = 'Rain';
+//                 break;
+
+//                 case 'Drizzle':
+//                     imgMain.src = 'img/rain.png';
+//                     mainWeather.innerHTML = 'Drizle';
+//                 break;
+
+//                 case 'Thunderstorm' :
+//                     imgMain.src = 'img/Thunderstorm.png';
+//                     mainWeather.innerHTML = 'Thunderstorm';
+//                 break;
+
+//                 case 'Fog' :
+//                     imgMain.src = 'img/fog.png';
+//                     mainWeather.innerHTML = 'Fog';
+//                 break;
+
+//                 case 'Mist' :
+//                     imgMain.src = 'img/fog.png';
+//                     mainWeather.innerHTML = 'Mist';
+//                 break;
+
+//                 case 'Haze' :
+//                     imgMain.src = 'img/fog.png';
+//                     mainWeather.innerHTML = 'Haze';
+//                 break; 
+
+//                 case 'Clear':
+//                     imgMain.src = 'img/clear.png';
+//                     mainWeather.innerHTML = 'Clear';
+//                 break;
+
+//                 case 'Snow':
+//                     imgMain.src = 'img/snow.png';
+//                     mainWeather.innerHTML = 'Snow';
+//                 break;
+                
+//                 default:
+//                     imgMain.src = '';   
+//             }
+//         }
+        
+//             mainTemp.innerHTML = `${Math.round(json.main.temp - 273.15)} °C`
+//             humidityValue.innerHTML = `${json.main.humidity} %`; 
+//             visibilityValue.innerHTML = `${Math.round(json.visibility / 1000)} km`;
+//             windValue.innerHTML = `${Math.round(json.wind.speed)} km/h`
+
+//             mainImg.style.display = ''; 
+//             mainTemp.style.display = '';
+//             weatherAdd.style.display = '';
+//     }); 
+// })
+ 
 //city search
    
     function searchClick() {
 
-        const APIkey = '80f6af54d5ac9494f7c6db394b035563'; 
-
         const location = searchInput.value; 
-
-        if(location === '') return;
+    
+        if(location == '') return;
         
+        const APIkey = '80f6af54d5ac9494f7c6db394b035563'; 
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`).then(response => response.json()).then(json => {
             
             if(json.cod === '404') {
@@ -146,6 +233,15 @@ theme.addEventListener("click", themeChange);
         }); 
         
     }
+    searchButton.addEventListener("click", () => {
+        searchClick(); 
+    }); 
+    document.addEventListener("keydown", event => {
+        if(event.code == "Enter") {
+            searchClick(); 
+        }
+     }); 
+    
 
   
 
